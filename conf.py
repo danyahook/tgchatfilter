1ec8564056977632dc50777ba5c49da9eba87c81
dkryuk <daniel.kryuk@websailors.pro>
2022-07-17 15:39:45 +0300

    Update ban pattern for 3.14*

--- conf.py
+++ conf.py
@@ -1,6 +1,6 @@
 class Config:
     CHANNEL_ID = -1001754918482
-    CENSOR_PATTERN = '[@#$%&*]'
+    CENSOR_PATTERN = '[@#$%&*,.]'
 
     BOT_API_TOKEN = 'TOKEN'
     TARGET_TG_ID = (444049905, )
@@ -9,9 +9,9 @@ class Config:
         'sp66fdc68dc48f640d86440992ce15db67_by_stckrRobot',
     )
 
-    STOP_PHRASES = ['bljat', 'ватник', '*internal screaming*', '*vine_boom.wav*', 'cушилка', 'не гей', 'не гомис',
-                    'не гомес', 'bruh', 'не отношусь', 'душнила']
+    STOP_PHRASES = ['bljat', 'ватник', '*internal screaming*', '*vine_boom.wav*', 'cушилка', 'bruh', 'не отношусь',
+                    'душнила', 'блетб', 'cук(']
 
-    STOP_CENSOR_PHRASES = ['3,14д', '3,14з', '3,14с', '3.14д', '3.14з', '3.14с', '6л', '6ь', 'fck', 'fuck', 'аним',
+    STOP_CENSOR_PHRASES = ['314д', '314с', '314з', '6л', '6ь', 'fck', 'fuck', 'аним',
                            'безю', 'бл', 'бь', 'г', 'дроч', 'е6', 'еб', 'жоп', 'ипа', 'муд', 'нй', 'пизд', 'секс',
                            'сук', 'хам', 'хер', 'хи', 'хре', 'ху', 'хя', 'член', 'ё6', 'ёб']
