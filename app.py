1849428105c2b7130a32b1892390c1f6c7dcba2e
dkryuk <daniel.kryuk@websailors.pro>
2022-08-16 23:33:00 +0300

    Add new command

--- app.py
+++ app.py
@@ -1,11 +1,16 @@
 import telebot as tb
 import logging
+import os
+import pytesseract
 import random as rnd
 import re
 import sys
 
+from datetime import datetime
+from io import BytesIO
 from telebot.types import Message
 from logging import StreamHandler, Formatter
+from PIL import Image
 
 from conf import Config
 
@@ -20,6 +25,8 @@ logger.addHandler(handler)
 
 
 def have_stop_phrase(message: str) -> bool:
+    if message is None:
+        return False
     message_text = re.sub(conf.CLEAN_SENTENCE_PATTERN, '', message.lower())
     message_words = set(message_text.split(' '))
 
@@ -32,6 +39,9 @@ def have_stop_phrase(message: str) -> bool:
         for word in message_words:
             if has_censor_symbol.search(word):
                 clean_word = re.sub(conf.CENSOR_PATTERN, '', word)
+                alt_set = set(clean_word) & set(conf.ALTERNATIVE_SYMBOL)
+                for letter in alt_set:
+                    clean_word = clean_word.replace(letter, conf.SOLDIER_MOD[letter])
                 if any(scw in clean_word for scw in conf.STOP_CENSOR_PHRASES):
                     return True
 
@@ -41,60 +51,107 @@ def have_stop_phrase(message: str) -> bool:
 def create_bot(api: str) -> tb.TeleBot:
     bot = tb.TeleBot(api)
 
+    @bot.message_handler(content_types=['photo'], func=lambda message: message.caption == '/send_img')
+    def send_image(message):
+        file_id = message.photo[-1].file_id
+        file_info = bot.get_file(file_id)
+        downloaded_file = bot.download_file(file_info.file_path)
+        image_caption = message.caption.removeprefix('/send_img')
+        bot.send_photo(chat_id=conf.SISYAN_ID, photo=downloaded_file, caption=image_caption)
+
+    @bot.message_handler(content_types=['text'], commands=['send_text'])
+    def send_text(message):
+        bot.send_message(chat_id=conf.SISYAN_ID, text=message.text.removeprefix('/send_text'))
+
     @bot.message_handler(commands=['soldier_mod'])
-    def handler_login(message):
+    def handler_soldier_mod(message):
         conf.SOLDIER_MOD = not conf.SOLDIER_MOD
         bot.send_message(message.from_user.id, f"Игорь Стрелков переведен в статус: {conf.SOLDIER_MOD}")
 
     @bot.message_handler(commands=['soldier_mod_status'])
-    def handler_login(message):
+    def handler_soldier_mod_status(message):
         bot.send_message(message.from_user.id, f"Игорь Стрелков в статусе: {bool(conf.SOLDIER_MOD)}")
 
+    @bot.message_handler(commands=['get_link'])
+    def get_invite_link(message):
+        link = bot.export_chat_invite_link(chat_id=conf.SISYAN_ID)
+        bot.send_message(message.from_user.id, link)
+
     @bot.message_handler(
-        func=lambda message: message.from_user.id in conf.TARGET_TG_ID,
+        func=lambda message: True,
         content_types=['text', 'sticker', 'photo', 'document']
     )
-    @bot.edited_message_handler(func=lambda message: message.from_user.id in conf.TARGET_TG_ID)
+    @bot.edited_message_handler(func=lambda message: True)
     def echo_message(message: Message) -> None:
-        if conf.SOLDIER_MOD:
-            if message.content_type == 'text':
-                message_text = re.sub(conf.CLEAN_SENTENCE_PATTERN, '', message.text.lower())
-                if message_text not in conf.SOLDIER_MOD_PHRASES:
-                    bot.delete_message(message.chat.id, message.message_id)
-                    bot.send_message(conf.CHANNEL_ID, f'🪖 <i>"{message.text}"</i>', parse_mode='HTML')
-                    bot.send_message(
-                        message.chat.id,
-                        rnd.choice(conf.BOT_SOLDIER_PROSES)
-                    )
-            elif message.content_type == 'photo':
-                bot.delete_message(message.chat.id, message.message_id)
-                bot.send_message(
-                    message.chat.id,
-                    rnd.choice(conf.BOT_SOLDIER_PROSES)
-                )
-            else:
-                bot.delete_message(message.chat.id, message.message_id)
-                bot.send_message(message.chat.id, 'Улыбышев! Отставить!')
-            return None
-
-        if message.content_type == 'sticker' and message.sticker.set_name in conf.IGNORED_STICKERS:
-            bot.delete_message(message.chat.id, message.message_id)
-            bot.send_message(conf.CHANNEL_ID, '📄')
-            bot.send_sticker(conf.CHANNEL_ID, message.sticker.file_id)
-            logger.info('sticker has been removed')
-        elif message.content_type == 'text':
+        logger.info(f'[MSG LOG] ({message.from_user.first_name}): {message.text}')
+        if message.from_user.id in conf.TARGET_TG_ID:
             event_type = 'edit' if message.edit_date else 'new'
 
-            logger.info('[%s] MSG: %s' % (event_type, message.text))
-            if have_stop_phrase(message.text):
+            if conf.SOLDIER_MOD:
+                if message.content_type == 'text':
+                    message_text = re.sub(conf.CLEAN_SENTENCE_PATTERN, '', message.text.lower())
+                    if message_text not in conf.SOLDIER_MOD_PHRASES:
+                        bot.delete_message(message.chat.id, message.message_id)
+                        bot.send_message(conf.CHANNEL_ID, f'🪖 <i>"{message.text}"</i>', parse_mode='HTML')
+                        if datetime.now() - conf.LAST_SOLDER_MESSAGE > conf.SOLDIER_MOD_SPAM_TIME:
+                            bot.send_message(
+                                message.chat.id,
+                                rnd.choice(conf.BOT_SOLDIER_PROSES)
+                            )
+                            conf.LAST_SOLDER_MESSAGE = datetime.now()
+                elif message.content_type == 'photo':
+                    bot.delete_message(message.chat.id, message.message_id)
+                    if datetime.now() - conf.LAST_SOLDER_MESSAGE > conf.SOLDIER_MOD_SPAM_TIME:
+                        bot.send_message(
+                            message.chat.id,
+                            rnd.choice(conf.BOT_SOLDIER_PROSES)
+                        )
+                        conf.LAST_SOLDER_MESSAGE = datetime.now()
+                else:
+                    bot.delete_message(message.chat.id, message.message_id)
+                    bot.send_message(message.chat.id, 'Улыбышев! Отставить!')
+                return None
+
+            if message.content_type == 'sticker' and message.sticker.set_name in conf.IGNORED_STICKERS:
                 bot.delete_message(message.chat.id, message.message_id)
-                bot.send_message(conf.CHANNEL_ID, f'<b>[{event_type}]</b>🤬 <i>"{message.text}"</i>', parse_mode='HTML')
-                logger.info('stop word was detected')
+                bot.send_message(conf.CHANNEL_ID, '📄')
+                bot.send_sticker(conf.CHANNEL_ID, message.sticker.file_id)
+                logger.info('sticker has been removed')
+            elif message.content_type == 'photo':
+                file_id = message.photo[-1].file_id
+                file_info = bot.get_file(file_id)
+                downloaded_file = bot.download_file(file_info.file_path)
+
+                image_stream = BytesIO(downloaded_file)
+                img = Image.open(image_stream)
+                img_text = pytesseract.image_to_string(img, config='--psm 4', lang='rus')
+                logger.info('[%s] MSG: %s' % (event_type, message.caption))
+                if have_stop_phrase(img_text):
+                    bot.delete_message(message.chat.id, message.message_id)
+                    bot.send_photo(conf.CHANNEL_ID, img, caption=f'<b>[{event_type}]</b>🤬📸', parse_mode='HTML')
+                    logger.info('stop word was detected on photo')
+                    return
+                elif have_stop_phrase(message.caption):
+                    bot.delete_message(message.chat.id, message.message_id)
+                    bot.send_message(conf.CHANNEL_ID, f'<b>[{event_type}]</b>🤬 <i>"{message.caption}"</i>',
+                                     parse_mode='HTML')
+                    logger.info('stop word was detected')
+
+            elif message.content_type == 'text':
+                logger.info('[%s] MSG: %s' % (event_type, message.text))
+                if have_stop_phrase(message.text):
+                    bot.delete_message(message.chat.id, message.message_id)
+                    bot.send_message(conf.CHANNEL_ID, f'<b>[{event_type}]</b>🤬 <i>"{message.text}"</i>',
+                                     parse_mode='HTML')
+                    logger.info('stop word was detected')
 
     return bot
 
 
 def main():
+    if not os.path.exists('tmp'):
+        os.mkdir('tmp')
+
     bot = create_bot(conf.BOT_API_TOKEN)
     bot.polling(none_stop=True, interval=0)
 
--- conf.py
+++ conf.py
@@ -1,23 +1,32 @@
+from datetime import datetime, timedelta
+
+
 class Config:
     CHANNEL_ID = -1001754918482
+    SISYAN_ID = -1001786724046
+    # SISYAN_ID = -1001678203307
     SOLDIER_MOD = 0
-    CENSOR_PATTERN = '[@#$%&*]'
-    CLEAN_SENTENCE_PATTERN = '[-,!?:;.]'
+    LAST_SOLDER_MESSAGE = datetime.now() - timedelta(minutes=1)
+    SOLDIER_MOD_SPAM_TIME = timedelta(minutes=1)
+    CENSOR_PATTERN = '[@#$%&*^]'
+    CLEAN_SENTENCE_PATTERN = '[-,[]|!?:;.]'
 
-    BOT_API_TOKEN = 'TOKEN'
-    TARGET_TG_ID = (316237594, )
-    # TARGET_TG_ID = (444049905, )
+    BOT_API_TOKEN = '5390283097:AAGgMJOSY99txo4TyblueDni8qmq02JMec4'
+    # TARGET_TG_ID = (316237594, )
+    TARGET_TG_ID = (444049905, )
 
     IGNORED_STICKERS = (
         'sp66fdc68dc48f640d86440992ce15db67_by_stckrRobot',
+        'SoyaCorp_ChatBubbles',
+        'floppa_chirpani',
     )
 
     STOP_PHRASES = ['bljat', 'ватник', '*internal screaming*', '*vine_boom.wav*', 'cушилка', 'bruh', 'не отношусь',
-                    'душнила', 'блетб', 'блеат', 'cук(', 'сук(' 'наwhyя', 'стицкер']
+                    'душнила', 'блетб', 'блеат', 'cук(', 'сук(' 'наwhyя', 'наwhyя', 'стицкер', '0)0']
 
-    STOP_CENSOR_PHRASES = ['314д', '314с', '314з', '6л', '6ь', 'fck', 'fuck', 'аним',
-                           'безю', 'бл', 'бь', 'г', 'дроч', 'е6', 'еб', 'жоп', 'ипа', 'муд', 'нй', 'пизд', 'секс',
-                           'сук', 'хам', 'хер', 'хи', 'хре', 'ху', 'хя', 'член', 'ё6', 'ёб']
+    STOP_CENSOR_PHRASES = ['314д', '314с', '314з','fck', 'fuck', 'аним',
+                           'безю', 'бл', 'бь', 'г', 'дроч', 'еб', 'жоп', 'ипа', 'муд', 'нй', 'пизд', 'секс',
+                           'сук', 'хам', 'хер', 'хи', 'хре', 'ху', 'xу', 'хy', 'xy', 'хя', 'член', 'ёб']
 
     SOLDIER_MOD_PHRASES = ['так точно', 'никак нет']
 
@@ -29,3 +38,7 @@ class Config:
         'Улыбышев, сто нарядов вне очереди! Отставить разговоры!\n"Так точно!" или "Никак нет!" и ни слова больше!',
         'Посмотри на себя, зеленый, женоподобный, безмускульный щенок гав-гав, к мамочке небось хочешь, домой, СИСЮ ХОЧЕШЬ? Отставить разговоры в строю!\n"Так точно!" или "Никак нет!" и ни слова больше!'
     ]
+
+    SYMBOL = ('х', 'у', 'б')
+    ALTERNATIVE_SYMBOL = ('x', 'y', '6')
+    SYMBOL_MAP = dict(zip(ALTERNATIVE_SYMBOL, SYMBOL))
